e271a145a038a05b592a377d38c5f5936718fa6b
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-21 17:50:34 -0700

    Initial Commit

---
+++ 24Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  24Todo
+//
+//  Created by Prudhvi Gadiraju on 3/21/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
