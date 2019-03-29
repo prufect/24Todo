56636ed9e03aa5d4e90923541d737b6734870f10
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-03-29 16:23:15 -0700

    - Added Item Model - Ability to Add Items - Ability to Search Items - Move View on Keyboard In and Out - Refactoring

---
+++ 24Todo/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  24Todo
+//
+//  Created by Prudhvi Gadiraju on 3/29/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    let title: String
+}
--- 24Todo/ViewController.swift
+++ 24Todo/ViewController.swift
@@ -11,58 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     let listHeaderView = UIView()
-    var listView: ListView!
-    let stackView = UIStackView()
+    var listTableView: ListView!
+    let listStackView = UIStackView()
     var stackViewLocation: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
-        
-        setupListHeaderView()
         setupListView()
-        setupStackView()
         setupGestureRecognizers()
+        setupNotificationObservers()
     }
+}
 
-    fileprivate func setupView() {
-        view.backgroundColor = .green
-    }
-    
-    fileprivate func setupListHeaderView() {
-        listHeaderView.backgroundColor = .yellow
-        
-        listHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        listHeaderView.heightAnchor.constraint(equalToConstant: 30).isActive = true
-    }
-    
-    
-    fileprivate func setupListView() {
-        listView = ListView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
-        //view.addSubview(listView)
-    }
-    
-    fileprivate func setupStackView() {
-        stackView.frame = CGRect(x: 0, y: view.frame.height*0.75, width: view.frame.width, height: view.frame.height)
-        stackView.axis = .vertical
-        
-        stackView.addArrangedSubview(listHeaderView)
-        stackView.addArrangedSubview(listView)
-        
-        view.addSubview(stackView)
-        
-    }
-    
-    fileprivate func setupGestureRecognizers() {
-        listHeaderView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture)))
-    }
-    
-    @objc
-    fileprivate func handlePanGesture(gesture: UIPanGestureRecognizer) {
+// MARK: - HandleFunctions
+extension ViewController {
+    @objc fileprivate func handlePanGesture(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .began:
-            stackViewLocation = stackView.center
+            stackViewLocation = listStackView.center
             print("Pan Began")
             break
         case .changed:
@@ -78,15 +46,76 @@ class ViewController: UIViewController {
     
     fileprivate func handlePanChanged(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: view)
-        print("Y: ", translation.y)
+        listStackView.center = CGPoint(x: stackViewLocation.x, y: stackViewLocation.y + translation.y)
         
-        stackView.center = CGPoint(x: stackViewLocation.x, y: stackViewLocation.y + translation.y)
+        //print("Y: ", translation.y)
+        //print(stackView.frame.minY)
     }
     
     fileprivate func handlePanEnded(gesture: UIPanGestureRecognizer) {
-       
         UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut, animations: {
-            self.stackView.transform = .identity
+            self.listStackView.transform = .identity
         }, completion: nil)
+    }
+    
+    @objc fileprivate func handleKeyboardShow(notification: Notification) {
+        // Only Move View when its lower then half the screen
+        if listStackView.frame.minY > view.frame.height / 2 {
+            guard let value = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {return}
+            let keyboardFrame = value.cgRectValue
+            
+            listStackView.transform = CGAffineTransform(translationX: 0, y: -keyboardFrame.height - 25)
+        }
+    }
+    
+    @objc fileprivate func handleKeyboardHide(notification: Notification) {
+        // Animate back to original position with Spring
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.listStackView.transform = .identity
+        }, completion: nil)
+    }
+}
+
+// MARK:- Setup Functions
+extension ViewController {
+    fileprivate func setupView() {
+        view.backgroundColor = .green
+    }
+    
+    fileprivate func setupListView() {
+        setupListHeaderView()
+        setupListTableView()
+        setupListStackView()
+    }
+    
+    fileprivate func setupListHeaderView() {
+        listHeaderView.backgroundColor = .yellow
+        
+        listHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        listHeaderView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+    }
+    
+    fileprivate func setupListTableView() {
+        listTableView = ListView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+    }
+    
+    fileprivate func setupListStackView() {
+        listStackView.frame = CGRect(x: 0, y: view.frame.height*0.75, width: view.frame.width, height: view.frame.height)
+        listStackView.axis = .vertical
+        
+        listStackView.addArrangedSubview(listHeaderView)
+        listStackView.addArrangedSubview(listTableView)
+        
+        view.addSubview(listStackView)
+        
+    }
+    
+    fileprivate func setupGestureRecognizers() {
+        listHeaderView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture)))
+    }
+    
+    fileprivate func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
